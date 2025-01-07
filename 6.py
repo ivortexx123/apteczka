879800588b3296b8a334362a9d5252882075945d
Igor <01197541@pw.edu.pl>
2025-01-07 18:35:08 +0100

    terminal interface

--- 6.py
+++ 6.py
@@ -94,6 +94,13 @@ class Lek:
 
 class LekPrzyjmowany:
     def __init__(self, lek, dawka, dzien):
+        if not isinstance(lek, Lek):
+            raise ValueError("Lek musi być instancją klasy Lek")
+        if not isinstance(dawka, str):
+            raise ValueError("Dawka musi być ciągiem znaków")
+        if not isinstance(dzien, str):
+            raise ValueError("Dzień musi być ciągiem znaków")
+
         self.lek = lek
         self.dawka = dawka
         self.dzien = dzien
@@ -101,12 +108,25 @@ class LekPrzyjmowany:
     def __str__(self):
         return f"{self.lek.nazwa} - Dawka: {self.dawka} - Dzień: {self.dzien}"
 
+
 class Uzytkownik:
     def __init__(self, imie, wiek, jednostki_chorobowe, uczulenia, leki_przyjmowane):
+        if not isinstance(imie, str):
+            raise ValueError("Imię musi być ciągiem znaków")
+        if not isinstance(wiek, int):
+            raise ValueError("Wiek musi być liczbą całkowitą")
+        if not isinstance(jednostki_chorobowe, list):
+            raise ValueError("Jednostki chorobowe muszą być listą")
+        if not isinstance(uczulenia, list):
+            raise ValueError("Uczulenia muszą być listą")
+        if not isinstance(leki_przyjmowane, list):
+            raise ValueError("Leki przyjmowane muszą być listą")
+
         self.imie = imie
         self.wiek = wiek
         self.jednostki_chorobowe = jednostki_chorobowe
         self.uczulenia = uczulenia
+
         # Sprawdzanie, czy leki przyjmowane są zgodne z klasą LekPrzyjmowany
         self.leki_przyjmowane = []
         for lek_przyjmowany in leki_przyjmowane:
@@ -122,6 +142,20 @@ class Uzytkownik:
             raise ValueError("Dodawany lek musi być instancją klasy LekPrzyjmowany")
 
     def edytuj(self, imie=None, wiek=None, jednostki_chorobowe=None, uczulenia=None, leki_przyjmowane=None):
+        if imie is not None and not isinstance(imie, str):
+            raise ValueError("Imię musi być ciągiem znaków")
+        if wiek is not None and not isinstance(wiek, int):
+            raise ValueError("Wiek musi być liczbą całkowitą")
+        if jednostki_chorobowe is not None and not isinstance(jednostki_chorobowe, list):
+            raise ValueError("Jednostki chorobowe muszą być listą")
+        if uczulenia is not None and not isinstance(uczulenia, list):
+            raise ValueError("Uczulenia muszą być listą")
+        if leki_przyjmowane is not None:
+            for lek_przyjmowany in leki_przyjmowane:
+                if not isinstance(lek_przyjmowany, LekPrzyjmowany):
+                    raise ValueError("Każdy element leki_przyjmowane musi być instancją klasy LekPrzyjmowany")
+            self.leki_przyjmowane = leki_przyjmowane
+
         if imie is not None:
             self.imie = imie
         if wiek is not None:
@@ -130,12 +164,6 @@ class Uzytkownik:
             self.jednostki_chorobowe = jednostki_chorobowe
         if uczulenia is not None:
             self.uczulenia = uczulenia
-        if leki_przyjmowane is not None:
-            for lek_przyjmowany in leki_przyjmowane:
-                if isinstance(lek_przyjmowany, LekPrzyjmowany):
-                    self.leki_przyjmowane = leki_przyjmowane
-                else:
-                    raise ValueError("Każdy element leki_przyjmowane musi być instancją klasy LekPrzyjmowany")
 
     def __str__(self):
         return (f"Użytkownik: {self.imie}\n"
@@ -144,28 +172,59 @@ class Uzytkownik:
                 f"Uczulenia: {', '.join(self.uczulenia)}\n"
                 f"Leki przyjmowane: {', '.join([str(lek_przyjmowany) for lek_przyjmowany in self.leki_przyjmowane])}\n")
 
+
 class Apteczka:
     def __init__(self):
         self.leki = []
         self.uzytkownicy = []
 
     def dodaj_lek(self, lek):
+        if not isinstance(lek, Lek):
+            raise ValueError("Dodawany lek musi być instancją klasy Lek")
         self.leki.append(lek)
 
     def usun_lek(self, nazwa_leku):
+        if not isinstance(nazwa_leku, str):
+            raise ValueError("Nazwa leku musi być ciągiem znaków")
+        poczatkowa_dlugosc = len(self.leki)
         self.leki = [lek for lek in self.leki if lek.nazwa != nazwa_leku]
+        if len(self.leki) == poczatkowa_dlugosc:
+            print(f"Leku o nazwie {nazwa_leku} nie znaleziono. Nic nie usunięto.")
 
     def znajdz_lek(self, nazwa_leku):
+        if not isinstance(nazwa_leku, str):
+            raise ValueError("Nazwa leku musi być ciągiem znaków")
         for lek in self.leki:
             if lek.nazwa == nazwa_leku:
                 return lek
         return None
 
     def dodaj_uzytkownika(self, uzytkownik):
+        if not isinstance(uzytkownik, Uzytkownik):
+            raise ValueError("Dodawany użytkownik musi być instancją klasy Uzytkownik")
         self.uzytkownicy.append(uzytkownik)
 
     def usun_uzytkownika(self, imie):
+        if not isinstance(imie, str):
+            raise ValueError("Imię użytkownika musi być ciągiem znaków")
+        poczatkowa_dlugosc = len(self.uzytkownicy)
         self.uzytkownicy = [uzytkownik for uzytkownik in self.uzytkownicy if uzytkownik.imie != imie]
+        if len(self.uzytkownicy) == poczatkowa_dlugosc:
+            print(f"Użytkownika o imieniu {imie} nie znaleziono. Nic nie usunięto.")
+
+    def sprawdz_bezpieczenstwo(self, lek, uzytkownik):
+        if not isinstance(lek, Lek):
+            raise ValueError("Lek musi być instancją klasy Lek")
+        if not isinstance(uzytkownik, Uzytkownik):
+            raise ValueError("Użytkownik musi być instancją klasy Uzytkownik")
+
+        if uzytkownik.wiek < lek.zalecany_wiek:
+            return False, f"Lek niebezpieczny ze względu na wiek (zalecany wiek: {lek.zalecany_wiek}, wiek użytkownika: {uzytkownik.wiek})"
+        for uczulenie in uzytkownik.uczulenia:
+            if uczulenie in lek.substancje_czynne:
+                return False, f"Lek niebezpieczny ze względu na uczulenie na substancje czynne ({uczulenie})"
+
+        return True, "Lek bezpieczny dla użytkownika"
 
     def wczytaj_baze(self, plik):
         with open(plik, newline='') as csvfile:
@@ -208,42 +267,130 @@ class Apteczka:
         else:
             print("Brak przeterminowanych leków.")
 
-    def sprawdz_bezpieczenstwo(self, lek, uzytkownik):
-        if uzytkownik.wiek < lek.zalecany_wiek:
-            return False
-        for uczulenie in uzytkownik.uczulenia:
-            if uczulenie in lek.substancje_czynne:
-                return False
-        return True
-
     def harmonogram_przyjmowania(self):
         for uzytkownik in self.uzytkownicy:
             print(f"\nHarmonogram przyjmowania leków dla {uzytkownik.imie}:")
             for lek_przyjmowany in uzytkownik.leki_przyjmowane:
-                print(f"{lek_przyjmowany['lek']} - Dawka: {lek_przyjmowany['dawka']} - Dzień: {lek_przyjmowany['dzien']}")
+                print(f"{lek_przyjmowany.lek.nazwa} - Dawka: {lek_przyjmowany.dawka} - Dzień: {lek_przyjmowany.dzien}")
 
 
+def main_menu():
+    apteczka = Apteczka()
 
-# Przykład użycia
-apteczka = Apteczka()
-lek1 = Lek("Paracetamol", "XYZ Pharma", ["gorączka", "ból"], ["mama", "tata"], ["paracetamol"], 12, 10, 8, "31-12-2024", "na gorączkę")
-lek2 = Lek("Ibuprofen", "ABC Pharma", ["ból", "zapal"], ["tata"], ["ibuprofen"], 12, 20, 20, "30-06-2025", "na ból")
+    while True:
+        print("\nMenu główne:")
+        print("1. Dodaj lek")
+        print("2. Usuń lek")
+        print("3. Dodaj użytkownika")
+        print("4. Usuń użytkownika")
+        print("5. Sprawdź bezpieczeństwo leku")
+        print("6. Wyświetl harmonogram przyjmowania leków")
+        print("7. Wczytaj bazę danych z pliku")
+        print("8. Zapisz bazę danych do pliku")
+        print("9. Wyjście")
+
+        wybor = input("Wybierz opcję (1-9): ")
+
+        if wybor == "1":
+            dodaj_lek(apteczka)
+        elif wybor == "2":
+            usun_lek(apteczka)
+        elif wybor == "3":
+            dodaj_uzytkownika(apteczka)
+        elif wybor == "4":
+            usun_uzytkownika(apteczka)
+        elif wybor == "5":
+            sprawdz_bezpieczenstwo(apteczka)
+        elif wybor == "6":
+            apteczka.harmonogram_przyjmowania()
+        elif wybor == "7":
+            plik = input("Podaj nazwę pliku do wczytania: ")
+            apteczka.wczytaj_baze(plik)
+        elif wybor == "8":
+            plik = input("Podaj nazwę pliku do zapisania: ")
+            apteczka.zapisz_baze(plik)
+        elif wybor == "9":
+            print("Do widzenia!")
+            break
+        else:
+            print("Nieprawidłowy wybór. Spróbuj ponownie.")
 
 
-uzytkownik1 = Uzytkownik("mama", 35, ["nadciśnienie"], ["paracetamol"], [{"lek": "Paracetamol", "dawka": "1 tabletka", "dzien": "poniedziałek"}])
-uzytkownik2 = Uzytkownik("tata", 40, ["cholesterol"], ["ibuprofen"], [{"lek": "Ibuprofen", "dawka": "1 tabletka", "dzien": "wtorek"}])
+def dodaj_lek(apteczka):
+    nazwa = input("Podaj nazwę leku: ")
+    producent = input("Podaj producenta: ")
+    jednostki_chorobowe = input("Podaj jednostki chorobowe (oddzielone średnikiem): ").split(';')
+    dla_kogo = input("Podaj osoby, dla których lek jest przeznaczony (oddzielone średnikiem): ").split(';')
+    substancje_czynne = input("Podaj substancje czynne (oddzielone średnikiem): ").split(';')
+    zalecany_wiek = int(input("Podaj zalecany wiek: "))
+    liczba_dawek = int(input("Podaj liczbę dawek: "))
+    liczba_dostepnych_dawek = int(input("Podaj liczbę dostępnych dawek: "))
+    termin_waznosci = input("Podaj termin ważności (dd-mm-YYYY): ")
+    notatka = input("Podaj notatkę: ")
+
+    lek = Lek(nazwa, producent, jednostki_chorobowe, dla_kogo, substancje_czynne, zalecany_wiek, liczba_dawek,
+              liczba_dostepnych_dawek, termin_waznosci, notatka)
+    apteczka.dodaj_lek(lek)
+    print("Lek został dodany.")
 
 
-apteczka.dodaj_lek(lek1)
-apteczka.dodaj_lek(lek2)
+def usun_lek(apteczka):
+    nazwa = input("Podaj nazwę leku do usunięcia: ")
+    apteczka.usun_lek(nazwa)
 
-print("Przed edycją:")
-print(lek1)
 
-# Edytowanie leku
-lek_do_edytowania = apteczka.znajdz_lek("Paracetamol")
-if lek_do_edytowania:
-    lek_do_edytowania.edytuj(producent="Nowy Producent", liczba_dostepnych_dawek=5)
+def dodaj_uzytkownika(apteczka):
+    imie = input("Podaj imię użytkownika: ")
+    wiek = int(input("Podaj wiek użytkownika: "))
+    jednostki_chorobowe = input("Podaj jednostki chorobowe (oddzielone średnikiem): ").split(';')
+    uczulenia = input("Podaj uczulenia (oddzielone średnikiem): ").split(';')
+    leki_przyjmowane = []
 
-print("Po edycją:")
-print(lek1)
+    dodaj_kolejny = input("Czy chcesz dodać leki przyjmowane? (tak/nie): ").lower()
+    while dodaj_kolejny == "tak":
+        nazwa_leku = input("Podaj nazwę leku: ")
+        lek = apteczka.znajdz_lek(nazwa_leku)
+        if lek:
+            dawka = input("Podaj dawkę: ")
+            dzien = input("Podaj dzień: ")
+            lek_przyjmowany = LekPrzyjmowany(lek, dawka, dzien)
+            leki_przyjmowane.append(lek_przyjmowany)
+        else:
+            print("Lek nie został znaleziony w apteczce.")
+
+        dodaj_kolejny = input("Czy chcesz dodać kolejny lek przyjmowany? (tak/nie): ").lower()
+
+    uzytkownik = Uzytkownik(imie, wiek, jednostki_chorobowe, uczulenia, leki_przyjmowane)
+    apteczka.dodaj_uzytkownika(uzytkownik)
+    print("Użytkownik został dodany.")
+
+
+def usun_uzytkownika(apteczka):
+    imie = input("Podaj imię użytkownika do usunięcia: ")
+    apteczka.usun_uzytkownika(imie)
+
+
+def sprawdz_bezpieczenstwo(apteczka):
+    nazwa_leku = input("Podaj nazwę leku: ")
+    imie_uzytkownika = input("Podaj imię użytkownika: ")
+
+    lek = apteczka.znajdz_lek(nazwa_leku)
+    uzytkownik = None
+    for user in apteczka.uzytkownicy:
+        if user.imie == imie_uzytkownika:
+            uzytkownik = user
+            break
+
+    if lek and uzytkownik:
+        bezpieczny, powod = apteczka.sprawdz_bezpieczenstwo(lek, uzytkownik)
+        if bezpieczny:
+            print("Lek jest bezpieczny dla użytkownika.")
+        else:
+            print(f"Lek nie jest bezpieczny: {powod}")
+    else:
+        print("Nie znaleziono leku lub użytkownika.")
+
+
+if __name__ == "__main__":
+    main_menu()
+
